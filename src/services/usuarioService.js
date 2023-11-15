d7b110317e4d71afe986696d478547850f9251e4
Felipe Souza <felipesoouza41@gmail.com>
2023-11-15 20:48:11 -0300

    fix { UsuariosService }

--- src/services/usuarioService.js
+++ src/services/usuarioService.js
@@ -23,7 +23,7 @@ class UsuarioService {
                 email: dto.email,
                 senha: senhaHash,
                 rolesId: dto.rolesId,
-                planosId: dto.planosId
+                planosId: dto.planosId,
             })
             return novoUsuario
         } catch (error) {
@@ -49,10 +49,13 @@ class UsuarioService {
                 nome: dto.nome,
                 sobrenome: dto.sobrenome,
                 email: dto.email,
+                rolesId: dto.rolesId,
+                planosId: dto.planosId,
+                status: dto.status,
             })
             return usuario
         } catch (error) {
-            console.log("Erro", error)
+            console.log('Erro', error)
             throw new Error('Erro ao atualizar usuário', error.message)
         }
     }
